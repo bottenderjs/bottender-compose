4a3871369f003fe881fa1179c8f0055f0d5e2e7c
kpman <s92f002@hotmail.com>
2018-03-21 12:03:53 +0800

    Handle parameter function

--- src/__tests__/sendText.spec.js
+++ src/__tests__/sendText.spec.js
@@ -10,3 +10,17 @@ it('should create action that will call sendText', async () => {
 
   expect(context.sendText).toBeCalledWith('haha');
 });
+
+it('should call parameter as function', async () => {
+  const mockFn = jest.fn();
+  mockFn.mockReturnValue('haha');
+  const action = sendText(mockFn);
+  const context = {
+    sendText: jest.fn(() => Promise.resolve()),
+  };
+
+  await action(context);
+
+  expect(context.sendText).toBeCalledWith('haha');
+  expect(mockFn).toBeCalled();
+});
